e5d9667238df071f94877604a3ecd576ae03cabf
ataussig <ataussig@gmail.com>
2014-01-26 14:50:50 -0800

    Finished Exercise 3.5.1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,6 +42,17 @@ describe "StaticPages" do
 		end
 	end
 
+	describe "Contact page" do
 
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+	end
 
 end
