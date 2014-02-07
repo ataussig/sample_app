a028aacac660934951afb76902802e48a21936c3
ataussig <ataussig@gmail.com>
2014-02-06 22:07:34 -0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
   # Enable all RESTful resources and actions: http://ruby.railstutorial.org/chapters/sign-up#table-RESTful_users
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root 'static_pages#home'
   match '/signup',    to: 'users#new',               via: 'get'
+  match '/signin',    to: 'sessions#new',            via: 'get'
+  match '/signout',   to: 'sessions#destroy',        via: 'delete'
   match '/help',      to: 'static_pages#help',       via: 'get'
   match '/about',     to: 'static_pages#about',      via: 'get'
   match '/contact',   to: 'static_pages#contact',    via: 'get'
